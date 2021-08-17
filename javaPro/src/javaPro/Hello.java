6d13fe53d9f7ccc3296001a30db201e6615d1253
redke <redke@KeNiK-T495>
2021-08-17 21:56:30 +0900

    2. hi 추가 코딩

--- javaPro/src/javaPro/Hello.java
+++ javaPro/src/javaPro/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~");
+		
+		// 새로운 팀원 A 추가 작업
+		System.out.println("hi~");
 
 	}
 
