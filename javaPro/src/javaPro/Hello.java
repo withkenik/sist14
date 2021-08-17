9ebd7a5b0f206747fda2aa9b48a755a159277701
redke <redke@KeNiK-T495>
2021-08-17 22:00:52 +0900

    팀원 A 다시 수정  - update hi

--- javaPro/src/javaPro/Hello.java
+++ javaPro/src/javaPro/Hello.java
@@ -6,8 +6,9 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~");
 		
-		// 새로운 팀원 A 추가 작업
-		System.out.println("hi~");
+		// 새로운 팀원 A 수정 작업
+		System.out.println("update hi~");
+		System.out.println("update hi~");
 
 	}
 
